6842217cb5c5973e2f13292016a947d19b878d2e
Jotaveesse <jvs2@cin.ufpe.br>
2025-03-17 16:38:37 -0300

    delay

--- src/test2.cpp
+++ src/test2.cpp
@@ -60,7 +60,7 @@ int main()
         lcd.clear();
         lcd.sendWrite(std::to_string(switchStates));
 
-        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        std::this_thread::sleep_for(std::chrono::milliseconds(400));
 
     }
 
