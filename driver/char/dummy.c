622517c0dfa6fb8aee896f406615f2408e9eb525
Jotaveesse <jvs@cin.ufpe.br>
2025-03-28 13:46:41 -0300

    l

--- driver/char/dummy.c
+++ driver/char/dummy.c
@@ -46,7 +46,7 @@ static struct class* my_class;
 static struct cdev my_device;
 
 #define DRIVER_NAME 	"my_driver"
-#define FILE_NAME 	"mydev"
+#define FILE_NAME 	"jvs"
 #define DRIVER_CLASS 	"MyModuleClass"
 
 /* fake device */
